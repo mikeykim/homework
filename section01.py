e9f69d3474603d6ed5709e9b8976e75d138a4446
Mikey <mkingg04@gmail.com>
2021-01-27 20:15:23 +0900

    Update section01.py

--- section01.py
+++ section01.py
@@ -3,17 +3,19 @@ from bs4 import BeautifulSoup
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://sports.news.naver.com/wfootball/record/index.nhn', headers=headers)
+data = requests.get('https://sports.news.naver.com/wfootball/record/index.nhn?category=epl&tab=team', headers=headers)
 soup = BeautifulSoup(data.text, 'html.parser')
 
-rank_table = soup.select('#wfootballTeamRecordBody > table > tbody> tr:nth-child(1)')
+rank_table = soup.select('')
+print(rank_table)
 
-for info in rank_table:
-    team_rank = info.select_one('td.num.best > div.inner > strong').text
-    team_name = info.select_one('td.align_l> div.inner > span.name').text
-    win_point = info.select_one('td.selected> div.inner > span').text
-    if float(win_point) > 30:
-        print(team_rank, team_name, win_point)
+##for info in rank_table:
+    ##team_rank = info.select_one('td.num.best > div.inner > strong').text
+        ##print(team_rank)
+    ##team_name = info.select_one('td.align_l> div.inner > span.name').text
+    ##win_point = info.select_one('td.selected> div.inner > span').text
+    ##if float(win_point) > 30:
+        ##print(team_rank, team_name, win_point)
 
 #container
 
@@ -22,3 +24,7 @@ for info in rank_table:
 #wfootballTeamRecordBody > table > tbody > tr:nth-child(1) > td.align_l > div > span
 
 # wfootballTeamRecordBody > table > tbody > tr:nth-child(1) > td.selected > div > span
+
+#wfootballTeamRecordBody > table > tbody > tr:nth-child(1) > td.align_l > div > span
+
+#wfootballTeamRecordBody > table > tbody > tr:nth-child(1) > td.num.best > div > strong
