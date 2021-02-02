2d8c8aab975c47f9bc2d0f2d4c1944f8d55fe73e
Mikey <mkingg04@gmail.com>
2021-02-02 22:18:59 +0900

    fixed

--- section01.py
+++ section01.py
@@ -1,30 +1,24 @@
 import requests
+import selenium
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://sports.news.naver.com/wfootball/record/index.nhn?category=epl&tab=team', headers=headers)
-soup = BeautifulSoup(data.text, 'html.parser')
+options = webdriver.ChromeOptions()
+options.headless = True
+options.add_argument(
+    "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
 
-rank_table = soup.select('')
-print(rank_table)
+driver = webdriver.Chrome('/Users/user/Downloads/chromedriver', options=options)
+driver.implicitly_wait(3)
+driver.get('https://sports.news.naver.com/wfootball/record/index.nhn?category=epl&tab=team')
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+rank_table = soup.select('#wfootballTeamRecordBody > table > tbody > tr')
 
-##for info in rank_table:
-    ##team_rank = info.select_one('td.num.best > div.inner > strong').text
-        ##print(team_rank)
-    ##team_name = info.select_one('td.align_l> div.inner > span.name').text
-    ##win_point = info.select_one('td.selected> div.inner > span').text
-    ##if float(win_point) > 30:
-        ##print(team_rank, team_name, win_point)
+for info in rank_table:
+    team_rank = info.select_one('td > div.inner > strong').text
 
-#container
-
-# wfootballTeamRecordBody > table > tbody > tr:nth-child(1) > td.num.best > div > strong
-
-#wfootballTeamRecordBody > table > tbody > tr:nth-child(1) > td.align_l > div > span
-
-# wfootballTeamRecordBody > table > tbody > tr:nth-child(1) > td.selected > div > span
-
-#wfootballTeamRecordBody > table > tbody > tr:nth-child(1) > td.align_l > div > span
-
-#wfootballTeamRecordBody > table > tbody > tr:nth-child(1) > td.num.best > div > strong
+    team_name = info.select_one('td.align_l> div.inner > span.name').text
+    win_point = info.select_one('td.selected> div.inner > span').text
+    if float(win_point) > 30:
+        print(team_rank, team_name, win_point)
